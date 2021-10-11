e81adbc42bc86877890277ee11a84c079f0e71c3
yindo <yinzhenzhi@tenwit.com>
2021-10-11 15:25:15 +0800

    增加app测试内容

--- gtav-appui/src/test/java/com/webframework/TestMKTPage.java
+++ gtav-appui/src/test/java/com/webframework/TestMKTPage.java
@@ -4,14 +4,13 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 public class TestMKTPage {
-    static MKTPage page ;
+    static MKTPage page;
     @Test
      void test() throws InterruptedException {
         page = new MKTPage();
         page.jumpToMorPaper().jumpToAddPaperPage().addNewsTest();
 //        Assert.assertNotNull(page.jumpToMorPaper().jumpToAddPaperPage().testAddPaperPage());
     }
-
     @AfterAll
     static void shutdown(){
         page.quit();
